27859e2d575b0fc58415be75352edb20ffb558f4
emon <arifin.emon.017@gmail.com>
2021-12-29 11:52:18 +0600

    my bootstarp project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW1vbjEzIiwiYSI6ImNrd2x0enc1eTFwdDcydG1kdDd3bWNxbXgifQ.Llz0PFiV8TvN6uf6MBjI5g';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position
+zoom: 9 // starting zoom
+});
+ 
